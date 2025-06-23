95c60a81a5bcf7909d2be6f3356d26d245f101f0
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-06-23 15:51:35 -0700

    Creating file route_helper_methods.py to store helper functions for card and board routes

---
+++ app/routes/route_helper_methods.py
@@ -0,0 +1,37 @@
+from flask import abort, make_response
+from ..db import db
+
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except ValueError:
+        response = {"details": f"{cls.__name__} id {model_id} is invalid"}
+        abort(make_response(response, 400))
+
+    query = db.select(cls).where(cls.id == model_id)
+    model = db.session.scalar(query)
+
+    if not model:
+        response = {"details": f"{cls.__name__} with id {model_id} not found"}
+        abort(make_response(response, 404))
+
+    return model
+
+
+def create_model_instance(cls, data):
+    try:
+        new_instance = cls.from_dict(data)
+    except KeyError:
+        response = {"details": "Invalid data"}
+        abort(make_response(response, 400))
+
+    db.session.add(new_instance)
+    db.session.commit()
+
+    return new_instance
+
+
+def create_model_response(cls, data):
+    new_instance = create_model_instance(cls, data)
+    key = cls.__name__.lower()
+    return {key: new_instance.to_dict()}, 201
