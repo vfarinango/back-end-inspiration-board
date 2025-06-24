15fad6ee2d1dab3d424a0af1ce1a23bb12f75e80
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-06-23 19:05:15 -0700

    Add final Board and Card models with relationships

--- app/models/board.py
+++ app/models/board.py
@@ -1,10 +1,12 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from ..db import db
 
 class Board(db.Model):
     board_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     owner: Mapped[str]
+    cards: Mapped[list["Card"]] = relationship(back_populates="board", cascade="all, delete-orphan")
+
 
     @classmethod
     def from_dict(cls, board_data):
@@ -15,7 +17,11 @@ class Board(db.Model):
 
     def to_dict(self):
         return {
-            "id": self.id,
+            "board_id": self.board_id,
             "title": self.title,
             "owner": self.owner
         }
+
+    # def update(self, data):
+    #     if "title" in data:
+    #         self.title = data["title"]
--- app/models/card.py
+++ app/models/card.py
@@ -1,23 +1,29 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from ..db import db
 
 class Card(db.Model):
     card_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     message: Mapped[str]
     likes_count: Mapped[int]
+    board_id: Mapped[int] = mapped_column(ForeignKey("board.board_id"), nullable=False)
+    board: Mapped["Board"] = relationship(back_populates="cards")
+
 
     @classmethod
     def from_dict(cls, card_data):
         return cls(
             message=card_data["message"],
-            likes_count=card_data["likes_count"]
+            likes_count=card_data["likes_count"],
+            board_id=card_data["board_id"] 
         )
     
     def to_dict(self):
         return {
-            "id": self.id,
+            "card_id": self.card_id,
             "message": self.message,
-            "likes_count": self.likes_count
+            "likes_count": self.likes_count,
+            "board_id": self.board_id
         }
     # def update(self, data):
     #     if "title" in data:
